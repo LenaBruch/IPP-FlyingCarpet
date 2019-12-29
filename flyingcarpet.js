74c7eaafbf0b9bd0f0ce9370882c34cb33ae5171
PhilEggman <philip.eiermann@googlemail.com>
2019-12-29 21:17:36 +0100

    Home Screen

---
+++ flyingcarpet.js
@@ -0,0 +1,50 @@
+/** 
+* Using the floating action button, the popup window for a new task will be shown
+*/
+
+function popupTask () {
+    $('#Popup_Task').show();
+}
+
+/** 
+* The popup window for a new task will disappear
+*/
+function popupTaskHide () {
+    $('#Popup_Task').hide();
+}
+
+/*function cityClicked () {
+    console.log($("#citytrip-rect").css("background-color"));
+    if ($('#citytrip_rect').css("background-color") == "rgba(230,230,230,1)"){
+        $('#citytrip_rect').css("background-color", "yellow");
+    }
+    //else{
+        // $('#citytrip_rect').css("background-color", "rgba(230,230,230,1)");
+}*/
+
+
+/** 
+* On the "Add Trip - Company" screen the "alone" radio button will be filled blue
+*/
+function alone () {
+    $('#ellipse_alone').css("fill", "rgba(0,188,212,1)");
+    $('#ellipse_with').css("fill", "white");
+}
+
+/** 
+* On the "Add Trip - Company" screen the "with" radio button will be filled blue
+*/
+function withFriend () {
+    $('#ellipse_with').css("fill", "rgba(0,188,212,1)");
+    $('#ellipse_alone').css("fill", "white");
+}
+
+/** 
+* On the home screen, you can mark the first task as done.
+*/
+function markTaskAsDone () {
+    $('#check1').css("fill", "rgba(0,188,212,1)");
+    $('#Rectangle_Date1').css("background-color", "darkgrey");
+    $('#task1_done').css("stroke", "darkgrey");
+}
+
